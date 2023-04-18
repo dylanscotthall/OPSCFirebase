dcdbffd2ca13e9e3209a8893eb7131e18af92838
dylanscotthall <dylanscotthall@gmail.com>
2023-04-18 06:55:33 +0000

    initial commit

---
+++ app/src/main/java/com/dylan/opscfirebaseauth/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.dylan.opscfirebaseauth
+
+import android.content.ContentValues.TAG
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        // get reference to button
+        val loginBtn = findViewById(R.id.LoginBtn) as Button
+        // set on-click listener
+        loginBtn.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
+        // get reference to button
+        val registerbtn = findViewById(R.id.RegisterBtn) as Button
+        // set on-click listener
+        registerbtn.setOnClickListener {
+            val email = findViewById<EditText>(R.id.editUsername)
+            val password = findViewById<EditText>(R.id.editPassword)
+            Log.d("stuff", email.text.toString());
+            createAccount(email.text.toString(), password.text.toString())
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+//        if(currentUser != null){
+//            reload()
+//        }
+    }
+
+    public fun createAccount(email: String, password: String){
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    //updateUI(null)
+                }
+            }
+
+    }
+
+    public fun singIn(email: String, password: String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    //updateUI(null)
+                }
+            }
+
+    }
+}
+
